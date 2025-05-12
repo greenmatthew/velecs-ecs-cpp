d3dbfb0c7e2d3824a89ada68935c9128c210e02a
Matthew Green <greenmatthew07@gmail.com>
2025-05-12 16:26:59 -0500

    Fixing file organization of include files. Also giving Entity .cpp and .hpp files content using templates.

---
+++ include/velecs/ecs/Entity.hpp
@@ -0,0 +1,46 @@
+/// @file    Entity.hpp
+/// @author  Matthew Green
+/// @date    2025-05-12 16:24:57
+/// 
+/// @section LICENSE
+/// 
+/// Copyright (c) 2025 Matthew Green - All rights reserved
+/// Unauthorized copying of this file, via any medium is strictly prohibited
+/// Proprietary and confidential
+
+#pragma once
+
+namespace velecs::ecs {
+
+/// @class Entity
+/// @brief Brief description.
+///
+/// Rest of description.
+class Entity {
+public:
+    // Enums
+
+    // Public Fields
+
+    // Constructors and Destructors
+
+    /// @brief Default constructor.
+    Entity() = default;
+
+    /// @brief Default deconstructor.
+    ~Entity() = default;
+
+    // Public Methods
+
+protected:
+    // Protected Fields
+
+    // Protected Methods
+
+private:
+    // Private Fields
+
+    // Private Methods
+};
+
+} // namespace velecs::ecs
---
+++ src/Entity.cpp
@@ -0,0 +1,29 @@
+/// @file    Entity.cpp
+/// @author  Matthew Green
+/// @date    2025-05-12 16:24:39
+/// 
+/// @section LICENSE
+/// 
+/// Copyright (c) 2025 Matthew Green - All rights reserved
+/// Unauthorized copying of this file, via any medium is strictly prohibited
+/// Proprietary and confidential
+
+#include "Entity.h"
+
+namespace velecs::ecs {
+
+// Public Fields
+
+// Constructors and Destructors
+
+// Public Methods
+
+// Protected Fields
+
+// Protected Methods
+
+// Private Fields
+
+// Private Methods
+
+} // namespace velecs::ecs

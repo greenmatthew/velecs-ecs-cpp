d5e0278803e86167931679b0d3e83dc54911f0c1
Matthew Green <greenmatthew07@gmail.com>
2025-05-13 16:25:52 -0500

    Added a common header file for easy #include for end user.

---
+++ include/velecs/ecs/Common.hpp
@@ -0,0 +1,21 @@
+/// @file    Common.hpp
+/// @author  Matthew Green
+/// @date    2025-05-13 16:21:51
+/// 
+/// @section LICENSE
+/// 
+/// Copyright (c) 2025 Matthew Green - All rights reserved
+/// Unauthorized copying of this file, via any medium is strictly prohibited
+/// Proprietary and confidential
+
+#pragma once
+
+#include "velecs/ecs/TypeConstraints.hpp"
+
+#include "velecs/ecs/SceneNode.hpp"
+#include "velecs/ecs/FolderEntity.hpp"
+#include "velecs/ecs/VirtualEntity.hpp"
+#include "velecs/ecs/Entity.hpp"
+
+#include "velecs/ecs/Component.hpp"
+#include "velecs/ecs/Transform.hpp"

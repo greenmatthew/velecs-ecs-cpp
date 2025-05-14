c49c4637354343661cddd7ef5367920d9fe6c354
Matthew Green <greenmatthew07@gmail.com>
2025-05-14 12:07:19 -0500

    Cleaning up and adding an assignment/copy operator as well as equality operators to Entity type.

--- include/velecs/ecs/Entity.hpp
+++ include/velecs/ecs/Entity.hpp
@@ -11,6 +11,7 @@
 #pragma once
 
 #include "velecs/ecs/TypeConstraints.hpp"
+#include "velecs/ecs/Component.hpp"
 
 #include <iostream>
 
@@ -30,44 +31,88 @@ public:
 
     static const Entity INVALID;
 
-    std::string name;
-
     // Constructors and Destructors
-
-    /// @brief Default constructor.
-    inline explicit Entity()
-        : handle(entt::null) {}
 
     /// @brief Default deconstructor.
     ~Entity() = default;
 
     // Public Methods
 
+    /// @brief Creates a new entity in the registry.
+    /// @return A newly created entity with a valid handle.
     static Entity Create();
 
+    /// @brief Copy assignment operator.
+    /// @param other The entity to copy from.
+    /// @return A reference to this entity after assignment.
+    inline Entity& operator=(const Entity& other)
+    {
+        if (this != &other) // Check for self-assignment
+        {
+            const_cast<entt::entity&>(handle) = other.handle; // Copy handle
+        }
+        return *this;
+    }
+
+    /// @brief Equality comparison operator.
+    /// @param other The entity to compare with.
+    /// @return True if the entities have the same handle, false otherwise.
+    inline bool operator==(const Entity& other) const
+    {
+        return handle == other.handle;
+    }
+
+    /// @brief Inequality comparison operator.
+    /// @param other The entity to compare with.
+    /// @return True if the entities have different handles, false otherwise.
+    inline bool operator!=(const Entity& other) const
+    {
+        return handle != other.handle;
+    }
+
+    /// @brief Checks if the entity is valid.
+    /// @return True if the entity handle is valid in the registry, false otherwise.
     inline bool IsValid() const
     {
         return registry.valid(handle);
     }
 
+    /// @brief Adds a component of type T to the entity.
+    /// @tparam T The type of component to add.
+    /// @return A reference to the newly added component.
     template<typename T, typename = IsComponent<T>>
     inline T& AddComponent()
     {
-        return registry.emplace<T>(handle);
+        T& comp = registry.emplace<T>(handle);
+        comp.owner = *this;
+        return comp;
     }
 
+    /// @brief Adds a component of type T to the entity with constructor arguments.
+    /// @tparam T The type of component to add.
+    /// @tparam Args The types of the constructor arguments.
+    /// @param args The constructor arguments.
+    /// @return A reference to the newly added component.
     template<typename T, typename = IsComponent<T>, typename... Args>
     inline T& AddComponent(Args &&...args)
     {
-        return registry.emplace<T>(handle, std::forward<Args>(args)...);
+        T& comp = registry.emplace<T>(handle, std::forward<Args>(args)...);
+        comp.owner = *this;
+        return comp;
     }
 
+    /// @brief Removes a component of type T from the entity.
+    /// @tparam T The type of component to remove.
     template<typename T, typename = IsComponent<T>>
     inline void RemoveComponent()
     {
         registry.remove<T>(handle);
     }
 
+    /// @brief Tries to get a component of type T from the entity.
+    /// @tparam T The type of component to get.
+    /// @param outComponent A pointer that will be set to the component if found.
+    /// @return True if the component was found, false otherwise.
     template<typename T, typename = IsComponent<T>>
     inline bool TryGetComponent(T*& outComponent)
     {
@@ -75,6 +120,10 @@ public:
         return (outComponent != nullptr);
     }
 
+    /// @brief Tries to get a const component of type T from the entity.
+    /// @tparam T The type of component to get.
+    /// @param outComponent A const pointer that will be set to the component if found.
+    /// @return True if the component was found, false otherwise.
     template<typename T, typename = IsComponent<T>>
     inline bool TryGetComponent(const T*& outComponent)
     {
@@ -82,8 +131,13 @@ public:
         return (outComponent != nullptr);
     }
 
+    /// @brief Requests an entity to be destroyed.
+    /// @details This adds the entity to a queue for destruction rather than destroying it immediately.
+    /// @param entity The entity to be queued for destruction.
     static void RequestDestroy(Entity entity);
 
+    /// @brief Processes all pending entity destruction requests.
+    /// @details Iterates through the destruction queue and destroys each entity.
     static void ProcessDestructionQueue();
 
 protected:
@@ -101,9 +155,18 @@ private:
 
     // Private Methods
 
+    /// @brief Default constructor.
+    /// @details Creates an entity with an invalid handle.
+    inline explicit Entity()
+        : handle(entt::null) {}
+
+    /// @brief Constructor with handle.
+    /// @param handle The entity handle to use.
     inline explicit Entity(entt::entity handle)
         : handle(handle) {}
 
+    /// @brief Destroys this entity.
+    /// @details Removes the entity from the registry.
     void Destroy();
 };
 

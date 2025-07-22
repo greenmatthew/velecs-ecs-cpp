615c08060b87ae5730bada5a643cd8635485b9c4
Matthew Green <greenmatthew07@gmail.com>
2025-07-22 13:27:29 -0500

    Added doc comments to Component class and also reorganized headers and implementation files in CMakeLists.txt.

--- include/velecs/ecs/Component.hpp
+++ include/velecs/ecs/Component.hpp
@@ -21,9 +21,12 @@ namespace velecs::ecs {
 class Entity;
 
 /// @class Component
-/// @brief Brief description.
+/// @brief Abstract base class for all component types in the ECS system.
 ///
-/// Rest of description.
+/// Components represent data and behavior that can be attached to entities.
+/// All component types must inherit from this class to be used with the ECS.
+/// Components should contain data relevant to a specific aspect of an entity
+/// (e.g., Transform for position/rotation, MeshRenderer for visual representation).
 class Component {
     friend class Entity;
 
@@ -38,16 +41,42 @@ public:
     Component() = default;
 
     /// @brief Pure virtual destructor to make class abstract.
+    /// @details Ensures proper cleanup of derived component types and makes
+    ///          this class abstract so it cannot be instantiated directly.
     virtual ~Component() = 0;
 
     // Public Methods
 
+    /// @brief Gets the entity that owns this component instance.
+    /// @tparam T The concrete component type (must be specified explicitly)
+    /// @return The Entity that contains this component
+    /// @warning Template parameter deduction does not work automatically - you must
+    ///          specify the concrete component type explicitly when calling this method.
+    ///
+    /// @code
+    /// class Camera : public Component {
+    /// public:
+    ///     void SomeMethod() const {
+    ///         // Must specify the concrete type explicitly:
+    ///         auto entity = GetOwner<Camera>();
+    ///         
+    ///         // This will NOT work (template deduction fails):
+    ///         // auto entity = GetOwner(); // ERROR!
+    ///     }
+    /// };
+    /// @endcode
     template<typename T, typename = IsComponent<T>>
     Entity GetOwner() const
     {
         return GetOwner<T>(*(T*)this);
     }
 
+    /// @brief Static method to get the entity that owns a specific component instance.
+    /// @tparam T The concrete component type
+    /// @param comp Reference to the component instance
+    /// @return The Entity that contains the given component
+    /// @details This is the underlying implementation used by the instance method.
+    ///          Uses EnTT's to_entity function to efficiently find the owning entity.
     template<typename T, typename = IsComponent<T>>
     static Entity GetOwner(const T& comp)
     {
